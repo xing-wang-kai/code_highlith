62859cbc66243f9d8b8b3e61e5ecb8cbed3ad033
Kai Wang <kaiuchihaa@gmail.com>
2024-10-08 21:14:57 -0300

    modificado os cards

--- src/pages/community/community.tsx
+++ src/pages/community/community.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import CommunityProps from "./assets/props/Community.props";
 import Card from "./components/cards.components";
 import './assets/style/community.style.scss'
+import hearterPng from './assets/_img/heart.png';
+import commentPng from './assets/_img/comment.png';
+import authorPng from '../../../src/pages/components/header/assets/_img/login-photo.png'
 
 const Community:React.FC<CommunityProps> = ({cards}) => {
     return (
@@ -19,6 +22,16 @@ const Community:React.FC<CommunityProps> = ({cards}) => {
                 <div className='c'>
                     <h3  className='community_body--title'>{card.title}</h3>
                     <p  className='community_body--description'>{card.description}</p>
+                    <div className='community_body--resume'>
+                        <div className='community_body--resume-engajamento'>
+                            <div className='community_body--resume-engajamento-heater'><img src={hearterPng} alt="imagem de coração" /></div>
+                            <div className='community_body--resume-engajamento-comment'><img src={commentPng} alt="imagem de ballow comments" /></div>
+                        </div>
+                        <div className='community_body--author'>
+                                <img src={authorPng} alt="imagem do author" />
+                                <p>@kai_wang</p>
+                        </div>
+                    </div>
 
                 </div>
             </div>
